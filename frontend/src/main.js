28d22f777b09cab458e38de1e3135b7154e4afd8
Ernesto800 <ernesto.boza.delacruz@gmail.com>
2025-09-05 12:41:43 +0200

    fix: cambiando el archivo main.js y la carpeta traduction y moviendola a public 0.1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,26 +3,23 @@ import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import router from './router';
 
-async function setupApp() {
-  const messages = await fetch('/traductions/es.json')
-    .then(response => response.json())
-    .then(es => ({
-      es,
-      en: {} 
-    }));
+const messages = Object.fromEntries(
+  Object.entries(import.meta.glob('./traductions/*.json', { eager: true }))
+    .map(([key, value]) => {
+      const locale = key.split('/').pop().replace('.json', '');
+      return [locale, value.default];
+    })
+);
 
-  const i18n = createI18n({
-    locale: 'es',
-    legacy: false,
-    fallbackLocale: 'en',
-    messages,
-  });
+const i18n = createI18n({
+  locale: 'es',
+  legacy: false,
+  fallbackLocale: 'en',
+  messages,
+});
 
-  const app = createApp(App);
-  app.use(router);
-  app.use(i18n);
+const app = createApp(App);
+app.use(router);
+app.use(i18n);
 
-  app.mount('#app');
-}
-
-setupApp();
+app.mount('#app');
