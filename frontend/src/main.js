d99acb6613d5c22073c1350517a01ff565761596
Ernesto800 <ernesto.boza.delacruz@gmail.com>
2025-09-05 13:04:49 +0200

    fix: cambiando el archivo main.js y la carpeta traduction y moviendola a public 0.7

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,34 +3,21 @@ import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import router from './router';
 
-async function setupApp() {
-  const i18n = createI18n({
-    locale: 'es',
-    legacy: false,
-    fallbackLocale: 'en',
-    messages: {
-      es: {},
-      en: {}
-    }
-  });
+import es from './traductions/es.json';
+import en from './traductions/en.json';
 
-  try {
-    const response = await fetch('/traductions/es.json');
-    if (response.ok) {
-      const messages = await response.json();
-      i18n.global.setLocaleMessage('es', messages);
-    } else {
-      console.error('Error al cargar la traducción:', response.statusText);
-    }
-  } catch (error) {
-    console.error('No se pudo cargar la traducción', error);
+const i18n = createI18n({
+  locale: 'es',
+  legacy: false,
+  fallbackLocale: 'en',
+  messages: {
+    es,
+    en
   }
+});
 
-  const app = createApp(App);
-  app.use(router);
-  app.use(i18n);
+const app = createApp(App);
+app.use(router);
+app.use(i18n);
 
-  app.mount('#app');
-}
-
-setupApp();
+app.mount('#app');
